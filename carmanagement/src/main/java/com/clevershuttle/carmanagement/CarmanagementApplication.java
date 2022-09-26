1c39e3576eefc6327bf96341ed9ce347eb77a0d9
nagsen1 <prince.nagsen@gmail.com>
2022-09-26 05:25:26 +0200

    first checkin with all the projects structure

---
+++ carmanagement/src/main/java/com/clevershuttle/carmanagement/CarmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.clevershuttle.carmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmanagementApplication.class, args);
+	}
+
+}
